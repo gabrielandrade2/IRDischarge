059134d1c9bc9555e13668f8abef091a2503dfc1
SiKeiRa <gus_rsiqueira@hotmail.com>
2013-05-07 13:25:08 -0300

    Adiciona teste de Análise Sintática. MainEtiquetas

--- src/console/MainEtiquetas.java
+++ src/console/MainEtiquetas.java
@@ -14,6 +14,7 @@ public class MainEtiquetas {
 		 * 
 		 */
 		
+		
 		// Altere aqui o sumï¿½rio que deseja etiquetar/classificar
 		//String text = "Paciente portador de prï¿½tese mecanica mitral normofuncionante, fibrilaï¿½ï¿½o atrial cronica, insuficiencia tricuspide grave, insuficiencia cardiaca esquerda e direita, FE 40%, interna por dispnï¿½ia ao repouso e congestï¿½o sistemica importante. Iniciado manejo com diurï¿½tico EV + HCTZ com boa reposta. Realizado cat cardiaco direito com POAP = 32 e PSAP = 36. Apresentou epistaxe recorrente com anticoagulaï¿½ï¿½o em nï¿½veis adequados -- avaliado pela otorrino = vasos proeminentes em plexo de Kiesselbach bilateralmente, sendo cauterizado. Tem alta hospitalar em bom estado geral, ICC classe III, com indicaï¿½ï¿½o de seguir acompanhamento no ambulatorio de ICT, anticoagulados e otorrino.   Orientaï¿½ï¿½es na alta Fazer uso das medicaï¿½ï¿½es conforme prescrito Restriï¿½ï¿½o hï¿½drica diaria a 1200 ml.  Dieta com pouco sal Procurar a emergencia se necessï¿½rio  Medicaï¿½ï¿½es na alta AAS 100 mg/d Femprocumona 3 mg/d Hidralazina 25 mg 3x/d Isossorbida 40 mg 3x/d Furosemida 120 + 120 mg/d Digoxina 0,125 mg 2/2d Alopurinol 100 mg/d ";
 		String text = "Feito encaminhamento para mï¿½dico";
@@ -61,10 +62,16 @@ public class MainEtiquetas {
 			  */
 			
 			cogroo.tagger(sc);
+			/**
+			* CHUNKER: Realiza etiquetagem sintática da frase
+			*/
+			cogroo.chunker(sc);
+			 
 			tokens = sc.getTokens();
 			for (Token token : tokens)
-				System.out.print(/*token.getLexeme() +*/ "_" + token.getMorphologicalTag() + " ");
-			System.out.println();		
+			System.out.print(token.getLexeme() + "_" + token.getMorphologicalTag() + " - (" + token.getChunkTag() + ")");
+			//System.out.print(token.getLexeme() + "_" + token.getMorphologicalTag() + " ");
+			System.out.println(); 		
 		}
 	}
 }
