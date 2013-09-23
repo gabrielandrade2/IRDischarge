2610c097822c41f84d005556b83192c5a5c14331
fernando <fernando@fernando-PC.home>
2013-09-23 16:25:03 -0300

    colocado o sql de criação da tabela rapidminer

--- sql/BD v0.4.sql
+++ sql/BD v0.4.sql
@@ -2,5 +2,6 @@ CREATE TABLE `textorapidminer` (
   `datahora` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `texto` text,
   PRIMARY KEY (`datahora`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$;
 
+alter table intemed.textoRapidMiner add idTexto int;
